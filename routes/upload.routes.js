e64b6d4ee3ebdc9b178f56fa9bb3b78972e6b140
yebrai <ivansevv@gmail.com>
2023-01-24 19:53:50 +0100

    end point for send pdf download

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -9,6 +9,7 @@ router.post("/", upload.fields([{ name: "file", maxCount: Infinity }]), (req, re
   
   for (let i = 0; i < files.file.length; i++) {
     // Procesar archivo (ej: guardar en base de datos, mover a una carpeta)
+    console.log(files.file)
     pdfArr.push(files.file)
   }
   
@@ -26,9 +27,10 @@ router.get("/files", (req, res) => {
           if (err) {
             res.status(500).send("Error al leer el archivo");
           } else {
-            readFiles.push({ fileName: file, data, url: `/download/${file}` });
+            readFiles.push({ fileName: file, data, url: `/uploads/${file}` });
             if (readFiles.length === files.length) {
               // Enviar la lista de archivos al cliente
+              console.log(readFiles);
               res.status(200).json(readFiles)
             }
           }
@@ -38,23 +40,18 @@ router.get("/files", (req, res) => {
   });
 });
 
-// router.get("/files", (req, res) => {
-//   fs.readdir("uploads/", (err, files) => {
-//     if (err) {
-//       res.status(500).send("Error al obtener los archivos");
-//     } else {
-//       // Crear un arreglo vacio para guardar los archivos leidos
-//       const readFiles = [];
+router.get("/download/:pdfId", (req, res) => {
+  const {pdfId} = req.params
+  const files = req.files;
+  console.log(pdfId)
+  for (let i = 0; i < files.file.length; i++) {
+    // Procesar archivo (ej: guardar en base de datos, mover a una carpeta)
+    console.log(files.file)
+    pdfArr.push(files.file)
+  }
+  
+  res.status(200).json(pdfArr);
+});
 
-//       // Utilizar forEach para leer cada archivo y guardarlo en el arreglo
-//       files.forEach((file) => {
-//         fs.readFile(`uploads/${file}`, (err, data) => {
-//           if (err) {
-//             res.status(500).send("Error al leer el archivo");
-//           } else {
-//             readFiles.push({ fileName: file, data });
-//             if (readFiles.length === files.length) {
-//               // Enviar la lista de archivos al cliente
-//               res.json(read
 
 module.exports = router;
